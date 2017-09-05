8bd2ec525ec5d93127126bdf17aa6bee99145c4d
Per Larsson <per.larsson@autodesk.com>
2017-09-05 10:13:00 +0200

    Initialize swap chain.

--- src/luxa/renderer/renderer.c
+++ src/luxa/renderer/renderer.c
@@ -5,13 +5,25 @@
 #include <vulkan/vulkan.h>
 #include <string.h>
 
+typedef struct swap_chain {
+	lx_array_t *images;
+	lx_array_t *image_views;
+	VkSwapchainKHR handle;
+	VkPresentModeKHR present_mode;
+	VkSurfaceFormatKHR surface_format;
+	VkExtent2D extent;
+} swap_chain_t;
+
 typedef struct physical_device
 {
 	VkPhysicalDevice handle;
 	VkPhysicalDeviceProperties properties;
 	VkPhysicalDeviceFeatures features;
 	VkPhysicalDeviceMemoryProperties memory_properties;
+	VkSurfaceCapabilitiesKHR surface_capabilities;
 	lx_array_t *queue_family_properties;  // VkQueueFamilyProperties
+	lx_array_t *surface_formats;
+	lx_array_t *present_modes;
 	int graphics_queue_family_index;
 	int compute_queue_family_index;
 	int presentation_queue_family_index;
@@ -29,7 +41,9 @@ typedef struct vulkan_renderer
 {
 	lx_allocator_t *allocator;
 	lx_array_t *physical_devices; // physical_device_t
+	physical_device_t* physical_device;
 	logical_device_t* device;
+	swap_chain_t *swap_chain;
 	VkInstance instance;
 	VkDebugReportCallbackEXT debug_report_extension;
 	VkSurfaceKHR presentation_surface;
@@ -51,11 +65,11 @@ VkBool32 debug_report_callback(
 
 lx_array_t *get_available_validation_layers(lx_allocator_t *allocator)
 {
-	uint32_t num_available_layers = 0;
-	vkEnumerateInstanceLayerProperties(&num_available_layers, NULL);
-	lx_array_t *layer_properties = lx_array_create_with_size(allocator, sizeof(VkLayerProperties), num_available_layers);
-	vkEnumerateInstanceLayerProperties(&num_available_layers, lx_array_begin(layer_properties));
-	return layer_properties;
+uint32_t num_available_layers = 0;
+vkEnumerateInstanceLayerProperties(&num_available_layers, NULL);
+lx_array_t *layer_properties = lx_array_create_with_size(allocator, sizeof(VkLayerProperties), num_available_layers);
+vkEnumerateInstanceLayerProperties(&num_available_layers, lx_array_begin(layer_properties));
+return layer_properties;
 }
 
 lx_array_t *get_available_extensions(lx_allocator_t *allocator)
@@ -81,7 +95,7 @@ lx_array_t *get_physical_devices(vulkan_renderer_t *renderer)
 	vkEnumeratePhysicalDevices(renderer->instance, &num_devices, available_physical_devices);
 
 	renderer->physical_devices = lx_array_create(renderer->allocator, sizeof(physical_device_t));
-	
+
 	for (uint32_t i = 0; i < num_devices; ++i) {
 		physical_device_t physical_device = { 0 };
 		physical_device.graphics_queue_family_index = -1;
@@ -89,20 +103,20 @@ lx_array_t *get_physical_devices(vulkan_renderer_t *renderer)
 		physical_device.presentation_queue_family_index = -1;
 
 		physical_device.handle = available_physical_devices[i];
-		
+
 		// Device properties
 		vkGetPhysicalDeviceProperties(physical_device.handle, &physical_device.properties);
 
 		// Features
 		vkGetPhysicalDeviceFeatures(physical_device.handle, &physical_device.features);
-		
+
 		// Memory properties
 		vkGetPhysicalDeviceMemoryProperties(physical_device.handle, &physical_device.memory_properties);
-		
+
 		// Queue family properties
 		uint32_t num_queue_family_properties = 0;
 		vkGetPhysicalDeviceQueueFamilyProperties(physical_device.handle, &num_queue_family_properties, NULL);
-		
+
 		if (num_queue_family_properties) {
 			physical_device.queue_family_properties = lx_array_create_with_size(renderer->allocator, sizeof(VkQueueFamilyProperties), num_queue_family_properties);
 			vkGetPhysicalDeviceQueueFamilyProperties(physical_device.handle, &num_queue_family_properties, lx_array_begin(physical_device.queue_family_properties));
@@ -122,6 +136,39 @@ lx_array_t *get_physical_devices(vulkan_renderer_t *renderer)
 				if (present_support) {
 					physical_device.presentation_queue_family_index = j;
 				}
+			}
+		}
+
+		// Surface capabilities
+		if (vkGetPhysicalDeviceSurfaceCapabilitiesKHR(physical_device.handle, renderer->presentation_surface, &physical_device.surface_capabilities) != VK_SUCCESS) {
+			LX_LOG_WARNING("Renderer", "Physical device has no surface capabilities");
+		}
+
+		// Surface formats
+		uint32_t num_surface_formats;
+		if (vkGetPhysicalDeviceSurfaceFormatsKHR(physical_device.handle, renderer->presentation_surface, &num_surface_formats, NULL) != VK_SUCCESS) {
+			LX_LOG_WARNING("Renderer", "Failed to get number of surface format(s)");
+		}
+
+		if (num_surface_formats) {
+			physical_device.surface_formats = lx_array_create_with_size(renderer->allocator, sizeof(VkSurfaceFormatKHR), num_surface_formats);
+
+			if (vkGetPhysicalDeviceSurfaceFormatsKHR(physical_device.handle, renderer->presentation_surface, &num_surface_formats, lx_array_begin(physical_device.surface_formats)) != VK_SUCCESS) {
+				LX_LOG_WARNING("Renderer", "Failed to get surface format(s)");
+			}
+		}
+
+		// Presentaton modes
+		uint32_t num_presentation_modes;
+		if (vkGetPhysicalDeviceSurfacePresentModesKHR(physical_device.handle, renderer->presentation_surface, &num_presentation_modes, NULL) != VK_SUCCESS) {
+			LX_LOG_WARNING("Renderer", "Failed to get number of presentation modes");
+		}
+
+		if (num_presentation_modes) {
+			physical_device.present_modes = lx_array_create_with_size(renderer->allocator, sizeof(VkPresentModeKHR), num_presentation_modes);
+			
+			if (vkGetPhysicalDeviceSurfacePresentModesKHR(physical_device.handle, renderer->presentation_surface, &num_presentation_modes, lx_array_begin(physical_device.present_modes)) != VK_SUCCESS) {
+				LX_LOG_WARNING("Renderer", "Failed to get presentation mode(s)");
 			}
 		}
 
@@ -254,17 +301,25 @@ lx_result_t initialize_physical_devices(vulkan_renderer_t *renderer)
 	lx_array_for_each(physical_device_t, p, physical_devices) {
 		LX_LOG_DEBUG("Renderer", "Found device, name=%s", p->properties.deviceName);
 	}
+
+	renderer->physical_device = lx_array_begin(renderer->physical_devices);
+	LX_LOG_DEBUG("Renderer", "Using %s as main physical device", renderer->physical_device->properties.deviceName);
 	
 	return LX_SUCCESS;
 }
 
-lx_result_t initialize_logial_devices(vulkan_renderer_t *renderer)
+lx_result_t initialize_logial_devices(
+	vulkan_renderer_t *renderer,
+	const char *extension_names[],
+	size_t num_extension_names,
+	const char *layer_names[],
+	size_t num_layer_names)
 {
 	LX_ASSERT(renderer, "Invalid renderer");
-	LX_ASSERT(lx_array_size(renderer->physical_devices), "No physical device(s)");
+	LX_ASSERT(renderer->physical_device, "No physical device(s)");
 
 	// TODO: Find the best GPU
-	physical_device_t *physical_device = lx_array_begin(renderer->physical_devices);
+	physical_device_t *physical_device = renderer->physical_device;
 	
 	VkPhysicalDeviceFeatures device_features = { 0 };
 
@@ -287,6 +342,10 @@ lx_result_t initialize_logial_devices(vulkan_renderer_t *renderer)
 	create_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
 	create_info.pQueueCreateInfos = queue_create_infos;
 	create_info.queueCreateInfoCount = physical_device->presentation_queue_family_index != physical_device->graphics_queue_family_index ? 2 : 1;
+	create_info.ppEnabledLayerNames = layer_names;
+	create_info.enabledLayerCount = (uint32_t)num_layer_names;
+	create_info.ppEnabledExtensionNames = extension_names;
+	create_info.enabledExtensionCount = (uint32_t)num_extension_names;
 	
 	VkDevice device;
 	VkResult result = vkCreateDevice(physical_device->handle, &create_info, NULL, &device);
@@ -304,23 +363,140 @@ lx_result_t initialize_logial_devices(vulkan_renderer_t *renderer)
 	return LX_SUCCESS;
 }
 
+lx_result_t initialize_swap_chain(vulkan_renderer_t *renderer)
+{
+	LX_ASSERT(renderer, "Invalid renderer");
+	LX_ASSERT(renderer->physical_device, "No physical device(s)");
+
+	physical_device_t *physical_device = renderer->physical_device;
+	
+	VkSurfaceFormatKHR surface_format = { 0 };
+	lx_array_for_each(VkSurfaceFormatKHR, sf, physical_device->surface_formats) {
+		if (sf->format == VK_FORMAT_B8G8R8A8_UNORM && sf->colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+			surface_format = (VkSurfaceFormatKHR) { .format = VK_FORMAT_B8G8R8A8_UNORM,.colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR };
+		}
+	}
+
+	if (surface_format.format == VK_FORMAT_UNDEFINED) {
+		surface_format = (VkSurfaceFormatKHR) { .format = VK_FORMAT_B8G8R8A8_UNORM, .colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR };
+	}
+	
+	VkPresentModeKHR present_mode = VK_PRESENT_MODE_FIFO_KHR;
+	lx_array_for_each(VkPresentModeKHR, pm, physical_device->present_modes) {
+		if (*pm == VK_PRESENT_MODE_MAILBOX_KHR) {
+			present_mode = VK_PRESENT_MODE_MAILBOX_KHR;
+			break;
+		}
+		else if (*pm == VK_PRESENT_MODE_IMMEDIATE_KHR) {
+			present_mode = VK_PRESENT_MODE_IMMEDIATE_KHR;
+			break;
+		}
+	}
+
+	VkExtent2D extent = physical_device->surface_capabilities.currentExtent;
+
+	uint32_t image_count = physical_device->surface_capabilities.minImageCount + 1;
+	if (physical_device->surface_capabilities.maxImageCount > 0 && image_count > physical_device->surface_capabilities.maxImageCount) {
+		image_count = physical_device->surface_capabilities.maxImageCount;
+	}
+	
+	VkSwapchainCreateInfoKHR create_info = { 0 };
+	create_info.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+	create_info.surface = renderer->presentation_surface;
+
+	create_info.minImageCount = image_count;
+	create_info.imageFormat = surface_format.format;
+	create_info.imageColorSpace = surface_format.colorSpace;
+	create_info.imageExtent = extent;
+	create_info.imageArrayLayers = 1;
+	create_info.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+
+	uint32_t queue_family_indicies[] = { (uint32_t)physical_device->graphics_queue_family_index, (uint32_t)physical_device->presentation_queue_family_index };
+
+	if (physical_device->graphics_queue_family_index != physical_device->presentation_queue_family_index) {
+		create_info.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+		create_info.queueFamilyIndexCount = 2;
+		create_info.pQueueFamilyIndices = queue_family_indicies;
+	}
+	else {
+		create_info.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+	}
+
+	create_info.preTransform = physical_device->surface_capabilities.currentTransform;
+	create_info.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+	create_info.presentMode = present_mode;
+	create_info.clipped = VK_TRUE;
+	create_info.oldSwapchain = VK_NULL_HANDLE;
+	
+	swap_chain_t *swap_chain = lx_alloc(renderer->allocator, sizeof(swap_chain_t));
+	*swap_chain = (swap_chain_t) { .images = 0, .image_views = 0, .present_mode = present_mode, .surface_format = surface_format, .extent = extent };
+	
+	if (vkCreateSwapchainKHR(renderer->device->handle, &create_info, NULL, &swap_chain->handle) != VK_SUCCESS) {
+		LX_LOG_ERROR("Renderer", "Failed to create swap chain");
+		return LX_ERROR;
+	}
+
+	// Get swap chain images
+	uint32_t num_images;
+	if (vkGetSwapchainImagesKHR(renderer->device->handle, swap_chain->handle, &num_images, NULL) != VK_SUCCESS) {
+		LX_LOG_ERROR("Renderer", "Failed to get number of swap chain image(s)");
+		return LX_ERROR;
+	}
+
+	swap_chain->images = lx_array_create_with_size(renderer->allocator, sizeof(VkImage), num_images);
+	if (vkGetSwapchainImagesKHR(renderer->device->handle, swap_chain->handle, &num_images, lx_array_begin(swap_chain->images)) != VK_SUCCESS) {
+		LX_LOG_ERROR("Renderer", "Failed to get swap chain image(s)");
+		return LX_ERROR;
+	}
+
+	// Create image views for each image
+	swap_chain->image_views = lx_array_create_with_size(renderer->allocator, sizeof(VkImageView), num_images);
+	VkImageView *image_view = lx_array_begin(swap_chain->image_views);
+	lx_array_for_each(VkImage, image, swap_chain->images) {
+		VkImageViewCreateInfo create_info = { 0 };
+		create_info.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+		create_info.image = *image;
+		create_info.viewType = VK_IMAGE_VIEW_TYPE_2D;
+		create_info.format = swap_chain->surface_format.format;
+		create_info.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+		create_info.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+		create_info.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+		create_info.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+		create_info.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+		create_info.subresourceRange.baseMipLevel = 0;
+		create_info.subresourceRange.levelCount = 1;
+		create_info.subresourceRange.baseArrayLayer = 0;
+		create_info.subresourceRange.layerCount = 1;
+
+		if (vkCreateImageView(renderer->device->handle, &create_info, NULL, image_view) != VK_SUCCESS) {
+			LX_LOG_WARNING("Renderer", "Failed to creat swap chain image view");
+		}
+
+		++image_view;
+	}
+
+	renderer->swap_chain = swap_chain;
+
+	return LX_SUCCESS;
+}
+
 lx_result_t lx_create_renderer(lx_allocator_t *allocator, lx_renderer_t **renderer, void* window_handle, void* module_handle)
 {
 	LX_ASSERT(allocator, "Invalid allocator");
 	LX_ASSERT(renderer, "Invalid renderer");
 
-	const char *validaton_layers[] = { "VK_LAYER_LUNARG_standard_validation" };
-	const uint32_t num_validation_layers = sizeof(validaton_layers) / sizeof(char*);
+	const char *layer_names[] = { "VK_LAYER_LUNARG_standard_validation" };
+	const uint32_t num_layer_names = sizeof(layer_names) / sizeof(char*);
 
-	const char *extensions[] = { "VK_KHR_surface", "VK_KHR_win32_surface", "VK_EXT_debug_report" };
-	const uint32_t num_extensions = sizeof(extensions) / sizeof(char*);
+	const char *extension_names[] = { VK_KHR_SURFACE_EXTENSION_NAME, VK_KHR_WIN32_SURFACE_EXTENSION_NAME, VK_EXT_DEBUG_REPORT_EXTENSION_NAME };
+	const uint32_t num_extension_names = sizeof(extension_names) / sizeof(char*);
 
 	vulkan_renderer_t *vulkan_renderer = lx_alloc(allocator, sizeof(vulkan_renderer_t));
 	memset(vulkan_renderer, 0, sizeof(vulkan_renderer_t));
 	vulkan_renderer->allocator = allocator;
 
 	// Initialize Vulkan instance
-	lx_result_t result = create_instance(vulkan_renderer, validaton_layers, num_validation_layers, extensions, num_extensions);
+	lx_result_t result = create_instance(vulkan_renderer, layer_names, num_layer_names, extension_names, num_extension_names);
 	if (result != LX_SUCCESS) {
 		lx_destroy_renderer(allocator, (lx_renderer_t*)vulkan_renderer);
 		return result;
@@ -354,7 +530,10 @@ lx_result_t lx_create_renderer(lx_allocator_t *allocator, lx_renderer_t **render
 	LX_LOG_DEBUG("Renderer", "Physical device(s) [OK]");
 
 	// Initialize logical device(s)
-	result = initialize_logial_devices(vulkan_renderer);
+	const char *device_extension_names[] = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
+	const uint32_t num_device_extension_names = sizeof(device_extension_names) / sizeof(char*);
+
+	result = initialize_logial_devices(vulkan_renderer, device_extension_names, num_device_extension_names, layer_names, num_layer_names);
 	if (result != LX_SUCCESS) {
 		LX_LOG_ERROR("Renderer", "Failed to initialize logical device(s)");
 		lx_destroy_renderer(allocator, (lx_renderer_t*)vulkan_renderer);
@@ -363,9 +542,15 @@ lx_result_t lx_create_renderer(lx_allocator_t *allocator, lx_renderer_t **render
 	LX_LOG_DEBUG("Renderer", "Logical device(s) [OK]");
 
 	// Initialize swap chain
-
+	result = initialize_swap_chain(vulkan_renderer);
+	if (result != LX_SUCCESS) {
+		LX_LOG_ERROR("Renderer", "Failed to initialize swap chain");
+		lx_destroy_renderer(allocator, (lx_renderer_t*)vulkan_renderer);
+		return result;
+	}
+	LX_LOG_DEBUG("Renderer", "Swap chain [OK]");
+	 
 	// Initialize image views
-
 	*renderer = (lx_renderer_t*)vulkan_renderer;
 	return LX_SUCCESS;
 }
@@ -377,10 +562,20 @@ void lx_destroy_renderer(lx_allocator_t *allocator, lx_renderer_t *renderer)
 
 	vulkan_renderer_t *vulkan_renderer = (vulkan_renderer_t*)renderer;
 
+	// Destroy swap chain
+	if (vulkan_renderer->swap_chain) {
+		vkDestroySwapchainKHR(vulkan_renderer->device->handle, vulkan_renderer->swap_chain->handle, NULL);
+		lx_array_destroy(vulkan_renderer->swap_chain->images);
+		lx_array_destroy(vulkan_renderer->swap_chain->image_views);
+		lx_free(vulkan_renderer->allocator, vulkan_renderer->swap_chain);
+		vulkan_renderer->swap_chain = NULL;
+	}
+	
 	// Destroy logical devices
 	if (vulkan_renderer->device) {
 		vkDestroyDevice(vulkan_renderer->device->handle, NULL);
 		lx_free(vulkan_renderer->allocator, vulkan_renderer->device);
+		vulkan_renderer->device = NULL;
 	}
 	
 	// Destroy physical devices
@@ -389,6 +584,7 @@ void lx_destroy_renderer(lx_allocator_t *allocator, lx_renderer_t *renderer)
 			lx_array_destroy(physical_device->queue_family_properties);
 		}
 		lx_array_destroy(vulkan_renderer->physical_devices);
+		vulkan_renderer->physical_devices = NULL;
 	}
 
 	// Destroy surface(s)
